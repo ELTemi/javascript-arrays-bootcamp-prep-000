39ad613716eba29d20e721e22c4774e6fe5c6fe5
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-03-26 17:00:19 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+function addElementToBeginningOfArray(myArray, newElement) {
+  return [newElement, ...myArray]
+}
+
+function destructivelyAddElementToBeginningOfArray(myArray, newElement) {
+  myArray.unshift(newElement)
+  return myArray
+}
+
+function addElementToEndOfArray(myArray, newElement) {
+  return [...myArray, newElement]
+}
+
+function destructivelyAddElementToEndOfArray(myArray, newElement) {
+  myArray.push(newElement)
+  return myArray
+}
